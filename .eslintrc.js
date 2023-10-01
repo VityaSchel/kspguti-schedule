0bb5f022e9a71555b6eb249fa48e4c69c1a515dd
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2023-10-01 18:53:04 +0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true
+  },
+  'extends': [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'indent': [
+      'warn',
+      2,
+      { SwitchCase: 1 }
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'warn',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'no-async-promise-executor': 'off'
+  }
+}
